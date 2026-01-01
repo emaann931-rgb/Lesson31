2589bdbe1bdffa82b0276cc48cba4bc54ed1b849
Noor13456 <noorgrps@gmail.com>
2026-01-01 12:17:51 +0500

    1

---
+++ activity1.py
@@ -0,0 +1,36 @@
+'''
+#to print numbers from 1 to 10 using for loop
+for i in range (1,11):
+    print(i)
+#to print num between 1 to 10 using while loops 
+i=1
+while i<=10:
+      print(i)
+      i=i+1
+
+#to print num between 50to 100 using while loops 
+i=50
+while i<=100:
+      print(i)
+      i=i+1
+ #to print num between 10 to 1 using while loops 
+i=10
+while i>=1:
+     print(i)
+     i=i-1
+
+#to print numbers from 50 to 100 using for loop
+for i in range (50,101):
+     print(i)
+
+#to print evennumbers  between 1 to 20 using for loop
+for i in range (1,21):
+    if i%2==0:
+         print(i)
+#to print evennumbers  between 1 to 20 using while loop
+i=1
+while i<=20:
+ if i%2==0:
+  print(i)
+i=i+1 
+'''
